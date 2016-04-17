ef49240ac4043acc3478c442f542827058b03f66
Ikeda Iori <m134_warrock_rpg7@yahoo.com>
2016-04-17 18:18:46 +0900

    Modify build and Apply .gitignore

---
+++ react_components/uipreprocess.jsx
@@ -0,0 +1,8 @@
+import LinearProgress from 'material-ui/lib/linear-progress';
+import CircularProgress from 'material-ui/lib/circular-progress';
+import RaisedButton from 'material-ui/lib/raised-button';
+import TextField from 'material-ui/lib/text-field';
+import FlatButton from 'material-ui/lib/flat-button';
+const buttonStyle = {
+  margin: 12,
+};
